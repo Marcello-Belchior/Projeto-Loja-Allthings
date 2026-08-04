cc7f9dad0cc30263a1a308ad9fd0529a6a4906cf
Marcello-Belchior <marcellinhobsf@gmail.com>
2026-08-04 19:29:11 -0300

    ProdutoController - Salvar, Criar, Listar

--- allthings/src/main/java/br/com/allthings/allthings/controller/ProdutoController.java
+++ allthings/src/main/java/br/com/allthings/allthings/controller/ProdutoController.java
@@ -1,7 +1,11 @@
 package br.com.allthings.allthings.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,25 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
-    //Método para salvar um produto
+    //  Método para salvar um produto
     @PostMapping("/salvar")
     public String salvar(@ModelAttribute Produto produto) {
         produtoService.save(produto);
         return "redirect:/produtos/listar";
+    }
+
+    //  Método para listar todos os produtos 
+    @GetMapping("/listar") 
+    public String listar(Model model) {
+        List<Produto> produtos= produtoService.findAll();
+        model.addAttribute("produtos", produtos);
+        return "aluno/listarProdutos"; 
+    }
+
+    //  Método para criar formulário
+    @GetMapping("/criar")
+    public String criarForm(Model model){
+        model.addAttribute("produto", new Produto());
+        return "produto/formularioProduto";
     }
 }
