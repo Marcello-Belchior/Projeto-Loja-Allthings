fe1b328cad53902002170a9970423c9f89e82825
Marcello Belchior da Silveira Filho <marcellinhobsf@gmail.com>
2026-08-18 20:31:00 -0300

    Delete allthings directory

--- allthings/src/main/java/br/com/allthings/allthings/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.allthings.allthings.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.allthings.allthings.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
-} 
--- allthings/src/main/java/br/com/allthings/allthings/repository/FornecedorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.allthings.allthings.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.allthings.allthings.entity.Fornecedor;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>{
-
-}
